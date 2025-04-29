e65301b130b305dce674f655d6fd004db694b04c
mael.pretet <mael.pretet1@mnhn.fr>
2025-04-29 16:58:49 +0200

    feat: ajout des programmes de requete de la base florilege + librairie

---
+++ programs/library.R
@@ -0,0 +1,9 @@
+# R 4.4.2
+library(cowplot) # 1.1.3
+suppressWarnings(suppressMessages(library(dplyr))) # 1.1.4
+library(ggplot2) # 3.5.1
+suppressWarnings(suppressMessages(library(here))) # 1.0.1
+suppressWarnings(suppressMessages(library(httr))) # 1.4.7
+library(jsonlite)
+suppressWarnings(suppressMessages(library(RMySQL))) # 0.10.29
+library(stringr) # 1.5.1
---
+++ programs/requete_florilege.R
@@ -0,0 +1,61 @@
+# HEADER --------------------------------------------
+#
+# Author:     Maël Pretet
+# Copyright     Copyright 2024 - Maël Pretet
+# Email:      mael.pretet1@mnhn.fr
+#
+# Date:     2025-03-24
+#
+# Script Name:    fonctions/requete_florilege.R
+#
+# Script Description:   Création du data frame opération papillons avec toutes
+#   les espèces. Interrogation de la base de données mosaic et enregistrement
+#   dans un fichier rds. Si le fichier existe déjà et qu'il date de moins d'une
+#   semaine, celui-ci est directement lu.
+#
+#
+# ------------------------------------
+
+library(dplyr)
+library(here)
+library(httr)
+library(readr)
+
+if (Sys.getenv("CI") != "true") {
+  readRenviron(".env")
+}
+source("programs/function_import_from_mosaic.R")
+is_intranet <- function(url = "https://virtualianet.mnhn.fr/") {
+  tryCatch({
+    response <- GET(url)
+    return(status_code(response) == 200)
+  }, error = function(e) {
+    return(FALSE)
+  })
+}
+
+### Dataframe des données pour toutes les espèces
+# -----------------------------------------------
+
+
+if (!file.exists("data/rdata/df_florilege.rds") & !is_intranet()) {
+  stop("Le fichier n'existe pas et ne peut pas être créé.")
+}else if ((!file.exists("data/rdata/df_florilege.rds") |                                 # Si le fichier n'existe pas OU
+          (strftime(Sys.Date(), "%A") == "lundi" &                                     #  [que la date du jour est un lundi ET
+           Sys.Date()-as.Date(file.info("data/rdata/df_florilege.rds")$ctime) > 5)) &    #   que le fichier a plus de 5 jours]
+          is_intranet()) {    
+  # Lecture depuis la base mosaic
+  df_florilege = import_from_mosaic(query = read_sql_query("sql/florilege_export_a_plat_standard.sql"),
+                                  database_name = "espaces_verts")
+  
+  # On sauvegarde si on ne se trouve pas sur le serveur gitlab
+  if (Sys.getenv("CI") != "true") {
+    # Sauvegarde du df en format RDS
+    saveRDS(object = df_florilege, file = "data/rdata/df_florilege.rds")
+  }
+  
+}else{
+  # Lecture du fichier RDS
+  df_florilege = readRDS("data/rdata/df_florilege.rds")
+}
+
